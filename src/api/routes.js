b5b2e032e2b8870147378016612c0f3c5e3ab928
alvintnw <alvin.tnw@gmail.com>
2025-04-20 10:37:46 +0700

    Update routes.js

--- src/api/routes.js
+++ src/api/routes.js
@@ -5,7 +5,12 @@ const works = require('./components/works/works-route');
 const mybooks = require('./components/mybooks/mybooks-route');
 const covers = require('./components/covers/covers-route');
 const changes = require('./components/changes/changes-route');
-
+const authors = require('./components/authors/authors-works-route');
+const subjects = require('./components/subjects/subjects-route');
+const editions = require('./components/editions/editions-route');
+const lists = require('./components/lists/lists-route');
+const searchinside = require('./components/searchinside/search-inside-route');
+const read = require('./components/read/read-route');
 
 module.exports = () => {
   const app = express.Router();
@@ -15,6 +20,12 @@ module.exports = () => {
   mybooks(app);
   covers(app);
   changes(app);
+  authors(app);
+  subjects(app);
+  editions(app);
+  lists(app);
+  searchinside(app);
+  read(app);
 
   return app;
 };
