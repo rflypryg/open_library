5dac0d7a5deee639215b496cf60ff36eb40fe19a
alvintnw <alvin.tnw@gmail.com>
2025-04-20 10:30:29 +0700

    Delete src/api/components/read-route.js

--- src/api/components/read-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const readController = require('./read-controller');
-
-module.exports = (app) => {
-  // Get book read data
-  app.get('/books/:id/read', readController.getBookReadData);
-  app.get('/books/:id/read.json', readController.getBookReadData);
-};
